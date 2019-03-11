028719ab0ea9cefcde8f93e67da814e53b615392
Vitor Emanuel <veslima3@gmail.com>
2019-03-11 12:52:34 -0300

    Chaining If Else Statements
    if/else statements can be chained together for complex logic. Here is pseudocode of multiple chained if / else if statements:
    
    if (condition1) {
      statement1
    } else if (condition2) {
      statement2
    } else if (condition3) {
      statement3
    . . .
    } else {
      statementN
    }
    
    Write chained if/else if statements to fulfill the following conditions:
    
    num < 5 - return "Tiny"
    num < 10 - return "Small"
    num < 15 - return "Medium"
    num < 20 - return "Large"
    num >= 20 - return "Huge"
    
    ps: return "false" wasn't necessary

--- Chaining If Else Statements.js
+++ Chaining If Else Statements.js
@@ -4,27 +4,24 @@ function testSize(num) {
     return "Tiny";
   }
 
-  else if (num < 10) {
+   else if (num < 10) {
     return "Small";
   }
 
-  else if (num < 15) {
+   else if (num < 15) {
     return "Medium";
   }
 
-  else if (num < 20) {
+   else if (num < 20) {
     return "Large";
   }
 
-  else if (num >= 20) {
+   else if (num >= 20) {
     return "Huge";
   }
-  
-  else {
-    return "False";
-  }
+
   // Only change code above this line
 }
 
-// Change this value to test
-console.log(testSize(7));
+ // Change this value to test
+console.log(testSize(1));
